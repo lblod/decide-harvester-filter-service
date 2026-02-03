725ae5011c65b6ae901aff6c6e50ed26dcdea1e5
Martijn Bogaert <martijn.bogaert@sirus.be>
2026-02-03 11:37:40 +0100

    Fix typo

--- constant.js
+++ constant.js
@@ -31,7 +31,7 @@ export const PREFIXES = `
 
 export const HIGH_LOAD_DATABASE_ENDPOINT =
   process.env.HIGH_LOAD_DATABASE_ENDPOINT || "http://database:8890/sparql";
-export const INPUT_GRAPH = process.env.INPUT_GRAPH_GRAPH || "http://mu.semte.ch/graphs/public";
+export const INPUT_GRAPH = process.env.INPUT_GRAPH || "http://mu.semte.ch/graphs/public";
 
 export const PUBLISHER_URI =
   process.env.PUBLISHER_URI || "http://data.lblod.info/services/decide-harvester-filter-service";
