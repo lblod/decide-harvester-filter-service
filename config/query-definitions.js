0440982dfdf26c1209e2e45ecb958931c07dc2dc
Martijn Bogaert <martijn.bogaert@sirus.be>
2026-02-05 14:30:31 +0100

    Remove commented query defs

--- config/query-definitions.js
+++ config/query-definitions.js
@@ -11,26 +11,4 @@ export const queryDefs = {
     bestuursorgaanPropertyPath:
       "^prov:generated / dcterms:subject / ^besluit:behandelt / besluit:isGehoudenDoor",
   },
-  // agendapunt: {
-  //   type: "besluit:Agendapunt",
-  //   bestuursorgaanPropertyPath: "^besluit:behandelt / besluit:isGehoudenDoor",
-  // },
-  // behandelingVanAgendapunt: {
-  //   type: "besluit:BehandelingVanAgendapunt",
-  //   bestuursorgaanPropertyPath: "dcterms:subject / ^besluit:behandelt / besluit:isGehoudenDoor",
-  // },
-  // bestuursorgaan: {
-  //   type: "besluit:Bestuursorgaan",
-  //   bestuursorgaanPropertyPath: "",
-  // },
-  // mandataris: {
-  //   type: "mandaat:Mandataris",
-  //   bestuursorgaanPropertyPath:
-  //     "^besluit:heeftVoorzitter / dcterms:subject / ^besluit:behandelt / besluit:isGehoudenDoor",
-  // },
-  // stemming: {
-  //   type: "besluit:Stemming",
-  //   bestuursorgaanPropertyPath:
-  //     "^besluit:heeftStemming / dcterms:subject / ^besluit:behandelt / besluit:isGehoudenDoor",
-  // },
 };
